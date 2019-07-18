2d7e8ffaa67fd75c8dd745a771e510e796374d56
Indunil1992 <indunil@adroitlogic.com>
2019-07-18 14:56:58 +0530

    10

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -1,20 +1,31 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+const ses = new AWS.SES();
 
 exports.handler = function (event, context, callback) {
 
-    sns.publish({
-        Message: 'c',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: ['sachithrarajapakse1992@gmail.com'],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: ''
+                }
+            },
+            Subject: {
+                Data: 'sub4'
+            }
+        },
+        Source: 'indunil@adroitlogic.com'
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
+
+
 
 
     callback(null, { "message": "Successfully API executed" });
