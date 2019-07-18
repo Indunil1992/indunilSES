14a5e04ab6738cbbd620827ab544384f3dfffa1f
Indunil1992 <indunil@adroitlogic.com>
2019-07-18 12:07:28 +0530

    a

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -1,8 +1,32 @@
-exports.handler = function(event, context, callback) {
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
+exports.handler = function (event, context, callback) {
+
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: ''
+                }
+            },
+            Subject: {
+                Data: 'subject 1'
+            }
+        },
+        Source: 'indunil@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
 
 
 
-    
-    callback(null, {"message": "Successfully API executed"});
+    callback(null, { "message": "Successfully API executed" });
 }
