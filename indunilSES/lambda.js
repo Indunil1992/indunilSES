2bcf03b27967cce38540b27d524df25e485de401
Indunil1992 <indunil@adroitlogic.com>
2019-07-19 15:35:32 +0530

    88

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -2,23 +2,26 @@ let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 
 exports.handler = function (event, context, callback) {
+
     ses.sendEmail({
         Destination: {
             ToAddresses: ['sachithrarajapakse1992@gmail.com'],
-            CcAddresses: [],
+            CcAddresses: ['indunil@adroitlogic.com'],
             BccAddresses: []
         },
         Message: {
             Body: {
-                Html: {
-                    Data: '<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a><i>Italic text.</i> <b>Bold text.</b> <big>Big text.</big> '
+                Text: {
+                    Data: 'Dear Amazon Web Services Customer,
+    
+    We have received a request to authorize this email address for use with Amazon SES and Amazon Pinpoint in region US East (N.Virginia).If you requested this verification, please go to the following URL to confirm that you are authorized to use this email address: '
                 }
             },
             Subject: {
-                Data: 'new html 1 '
+                Data: 'text c 1'
             }
         },
-        Source: 'indunil@adroitlogic.com'
+        Source: 'indunil@adroitlogic.com',
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
