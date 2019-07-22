444221025d3c51ae52b5d4d36af778529741002d
Indunil1992 <indunil@adroitlogic.com>
2019-07-22 10:42:24 +0530

    8

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -1,24 +1,23 @@
 let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 
-exports.handler = function (event, context, callback) {
 
+exports.handler = function (event, context, callback) {
     ses.sendEmail({
         Destination: {
-            ToAddresses: ['sachithrarajapakse1992@gmail.com'],
-            CcAddresses: ['indunil@adroitlogic.com'],
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: [],
             BccAddresses: []
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'Dear Amazon Web Services Customer,
-    
-    We have received a request to authorize this email address for use with Amazon SES and Amazon Pinpoint in region US East (N.Virginia).If you requested this verification, please go to the following URL to confirm that you are authorized to use this email address: '
-                }
+                    Data: 'Why do we use it?
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.The point of using Lorem Ipsum is that it has a more-or - less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.  '
+    				}
             },
             Subject: {
-                Data: 'text c 1'
+                Data: 'sub '
             }
         },
         Source: 'indunil@adroitlogic.com',
@@ -26,6 +25,7 @@ exports.handler = function (event, context, callback) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
     });
+
 
 
 
