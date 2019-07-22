1dcf1116752112c1c777a6444e257045e991f017
Indunil1992 <indunil@adroitlogic.com>
2019-07-22 12:02:21 +0530

    swa

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     ses.sendEmail({
         Destination: {
             ToAddresses: ['sachithrarajapakse1992@gmail.com'],
-            CcAddresses: ['indunil@adroitlogic.com', 'hirudinee@adroitlogic.com'],
+            CcAddresses: ['indunil@adroitlogic.com', 'hirudinee+@adroitlogic.com'],
             BccAddresses: []
         },
         Message: {
@@ -19,7 +19,7 @@ exports.handler = function (event, context, callback) {
                 Data: 'cc html'
             }
         },
-        Source: 'sachithrarajapakse1992@gmail.com',
+        Source: 'sachithrarajapakse1992@gmail.com'
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
