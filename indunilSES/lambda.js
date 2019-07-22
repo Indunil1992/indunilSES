8271fa75f04ccd82561aee1c25ee61b834c8f379
Indunil1992 <indunil@adroitlogic.com>
2019-07-22 13:20:15 +0530

    nkllujjjjjjjjjjj

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -1,25 +1,26 @@
 let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 
+
 exports.handler = function (event, context, callback) {
 
     ses.sendEmail({
         Destination: {
-            ToAddresses: ['sachithrarajapakse1992@gmail.com'],
-            CcAddresses: [],
-            BccAddresses: ['indunil@adroitlogic.com', 'hirudinee+aws@adroitlogic.com']
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: ['sachithrarajapakse1992@gmail.com', 'hirudinee+aws@adroitlogic.com'],
+            BccAddresses: ['indunil@adroitlogic.com', 'sachithrarajapakse1992@gmail.com']
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'If you have already been approved for a sending limit increase, then you can start sending email to non-verified addresses.'
+                    Data: 'We have received a request to authorize this email address for use with Amazon SES and Amazon Pinpoint in region US East (N. Virginia). If you requested this verification, please go to the following URL to confirm that you are authorized to use this email address:'
                 }
             },
             Subject: {
-                Data: 'BCC  html'
+                Data: 'check new cc and bcc'
             }
         },
-        Source: 'sachithrarajapakse1992@gmail.com'
+        Source: 'hirudinee+aws@adroitlogic.com'
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
