9d7514c763ecc3e52e4894a9a76d51bf2feb2191
Indunil1992 <indunil@adroitlogic.com>
2019-07-22 11:58:27 +0530

    879

--- indunilSES/lambda.js
+++ indunilSES/lambda.js
@@ -2,23 +2,24 @@ let AWS = require('aws-sdk');
 const ses = new AWS.SES();
 
 exports.handler = function (event, context, callback) {
+
     ses.sendEmail({
         Destination: {
-            ToAddresses: ['indunil@adroitlogic.com'],
-            CcAddresses: [],
+            ToAddresses: ['sachithrarajapakse1992@gmail.com'],
+            CcAddresses: ['indunil@adroitlogic.com', 'hirudinee@adroitlogic.com'],
             BccAddresses: []
         },
         Message: {
             Body: {
                 Text: {
-                    Data: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
+                    Data: '<i>Italic text.</i> <b>Bold text.</b> <big>Big text.</big>'
                 }
             },
             Subject: {
-                Data: 'sub text'
+                Data: 'cc html'
             }
         },
-        Source: 'indunil@adroitlogic.com',
+        Source: 'sachithrarajapakse1992@gmail.com',
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
